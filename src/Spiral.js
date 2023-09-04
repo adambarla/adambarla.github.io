4b91f8e6aa701ab2da03a1847658590b20ae797a
Adam Barla <barlaada@fit.cvut.cz>
2023-09-04 22:46:49 +0200

    automatic detection for number of primes needed in the spiral to cover the whole screen

--- src/Spiral.js
+++ src/Spiral.js
@@ -1,43 +1,49 @@
-import React, {useCallback, useEffect, useRef} from "react";
+import React, {useCallback, useEffect, useMemo, useRef, useState} from "react";
 import p5 from "p5";
 import {animated, useSpring} from "@react-spring/web";
-
-const n = 110000;
 
 export const Spiral = () => {
     // let t = 1;
     const t = useRef(Math.random() * 2 * Math.PI);
-    const primes = useRef(new Map());
     const p5Ref = useRef(null);
+    const [width, setWidth] = useState(0);
+    const [height, setHeight] = useState(0);
 
+    const primes = useMemo(() => {
+        let tmp = new Map();
+        if (p5Ref.current === null) return tmp;
+
+        const n = Math.ceil(Math.sqrt(width ** 2 + height ** 2)) * 50;
+
+        for (let i = 2; i <= n; i++) {
+            if (!tmp.has(i)) {
+                tmp.set(i, true);
+                for (let j = i * i; j <= n; j += i) {
+                    tmp.set(j, false);
+                }
+            }
+        }
+        return tmp;
+    }, [width, height]);
 
     const sketch = useCallback((p) => {
         if (p5Ref.current === null) return;
         p.setup = () => {
-            p.createCanvas(p5Ref.current.offsetWidth, p5Ref.current.offsetHeight);
+            p.createCanvas(width, height);
             p.fill(125, 157, 223, 255);
             p.noStroke();
-
-            // Generate primes
-            for (let i = 2; i <= n; i++) {
-                if (!primes.current.has(i)) {
-                    primes.current.set(i, true);
-                    for (let j = i * i; j <= n; j += i) {
-                        primes.current.set(j, false);
-                    }
-                }
-            }
+            p.frameRate(30);
         };
 
         p.windowResized = () => {
-            p.resizeCanvas(p5Ref.current.offsetWidth, p5Ref.current.offsetHeight);
+            p.resizeCanvas(width, height);
         }
 
         p.draw = () => {
             p.background(7, 18, 48, 255);
 
-            for (let prime of primes.current.keys()) {
-                if (primes.current.get(prime)) {
+            for (let prime of primes.keys()) {
+                if (primes.get(prime)) {
                     p.circle((prime * Math.sin(prime * t.current)) / 99 + p5Ref.current.offsetWidth / 2,
                         (prime * Math.cos(prime * t.current)) / 99 + p5Ref.current.offsetHeight / 2,
                         Math.max(2, p5Ref.current.offsetWidth / 500));
@@ -46,17 +52,32 @@ export const Spiral = () => {
 
             t.current += 0.0000001 / Math.max(p5Ref.current.offsetWidth, p5Ref.current.offsetHeight) * 1000;
         };
-    }, []);
+    }, [width, height, primes]);
+
 
     useEffect(() => {
         if (p5Ref.current === null) return;
 
-        console.log("seed :", t.current);
         const mp5 = new p5(sketch, p5Ref.current);
         return () => {
             mp5.remove();
         }
-    }, [p5Ref.current?.offsetWidth, p5Ref.current?.offsetHeight, sketch]);
+    }, [sketch]);
+
+    useEffect(() => {
+        console.log("seed :", t.current);
+
+        const observer = new ResizeObserver((entries) => {
+            const rect = entries[0].contentRect;
+            setWidth(rect.width);
+            setHeight(rect.height);
+        });
+        observer.observe(document.documentElement);
+
+        return () => {
+            observer.unobserve(document.documentElement);
+        };
+    }, []);
 
     const [props] = useSpring(
         () => ({
@@ -72,6 +93,7 @@ export const Spiral = () => {
 
     return (
         <animated.div style={props} className={"w-100 h-100 rounded-5 overflow-hidden bg-secondary"}
-                      ref={p5Ref}/>
+                      ref={p5Ref}>
+        </animated.div>
     );
 };
